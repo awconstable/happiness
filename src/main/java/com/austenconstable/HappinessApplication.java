1d1d5e3d503f32fb2a44077a7a53b2639e9d6db2
Austen Constable <austen@crossfire.ltd.uk>
2017-02-12 17:57:04 +0000

    Initial commit with spring boot web starter

---
+++ src/main/java/com/austenconstable/HappinessApplication.java
@@ -0,0 +1,12 @@
+package com.austenconstable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappinessApplication.class, args);
+	}
+}
