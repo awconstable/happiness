f540e232d86e6cab1e67e77f15d43fb9b27ae526
Austen Constable <awconstable@gmail.com>
2021-02-11 18:17:11 +0100

    Remove unnecessary escape sequence

--- web/src/main/resources/static/js/trendgraph.js
+++ web/src/main/resources/static/js/trendgraph.js
@@ -1,6 +1,6 @@
 
 $.urlParam = function(name){
-    var results = new RegExp('[\?&]' + name + '=([^&]*)').exec(window.location.href);
+    var results = new RegExp('[?&]' + name + '=([^&]*)').exec(window.location.href);
     if (results==null){
         return null;
     }
